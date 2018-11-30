ca949b26b7526678bf8d10caa09eb38fa51d8ee0
Fsy379 <874781460@qq.com>
2018-11-30 11:27:52 +0800

    header

---
+++ src/common/header.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import './headerstyle.scss'
+
+// 复杂组件,容器组件
+export default class header extends Component{
+    
+    render(){
+        return (
+        <div className="header"> <img className="logo" src="https://y.gtimg.cn/mediastyle/mobile/yqq_v5/img/logo.png?max_age=19830212&d=20151105145423" alt=""/></div>
+    )
+    }
+
+}
