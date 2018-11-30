dd7f520c195331af34ab8ca631bcd429b44a0238
cc12210 <2832913976@qq.com>
2018-11-30 19:16:29 +0800

    请求主页数据

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,31 @@
-import React, { Component } from 'react';
-import './Home.scss'
+import React, { Component } from "react";
+import "./Home.scss";
+import "whatwg-fetch";
 
 export default class Home extends Component {
-    constructor(params) {
-        super(params);
-    }
-    render() {
-        return (
-             <div className="page">
-                 <p>主页</p>
-                 <p>主页</p>
-                 <p>主页</p>
-                 <p>主页</p>
-                 <p>主页</p>
-             </div>
-        );
-    }
+  constructor(params) {
+    super(params);
+  }
+  render() {
+    return (
+      <div className="page">
+        <div>
+          <p>主页</p>
+          <p>主页</p>
+          <p>主页</p>
+          <p>主页</p>
+          <p>主页</p>
+        </div>
+      </div>
+    );
+  }
+  componentDidMount() {
+    fetch("/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg")
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+          console.log(res.data);
+      });
+  }
 }
-
