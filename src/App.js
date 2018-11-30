b7b4dbd4b19f33cc0833bef32ca514e8547797a5
cc12210 <2832913976@qq.com>
2018-11-30 10:24:58 +0800

    提交

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Rouer, Route, Link } from "react-router-dom";
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+  }
   render() {
     return (
-      <div className="App">
-        <h1>测试</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+
+        </div>
+      </BrowserRouter>
     );
   }
 }
